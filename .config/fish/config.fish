7ec0569c652eeda079497f7158510b749e89d549
Federico <federico@garro.local>
2022-08-03 15:31:04 +0200

    added Run:ai aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,16 @@ eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 ######################### ALIASES #######################	
-########## sshsf#######
+############ RUN:AI #####################################
+alias rd="runai delete"
+alias rdes="runai describe job"
+alias rb="runai bash"
 alias kg="kubectl get pods"
-alias r="runai"
+alias kc="kubectl create -f"
+alias ka="kubectl apply -f"
+alias kl="kubectl logs"
+
+########## sshsf#######
 alias mntcvlab="sudo diskutil umount force ~/EPFL ; sshfs garro@iccvlabsrv23.iccluster.epfl.ch:/ ~/EPFL -o auto_cache,reconnect,defer_permissions,noappledouble,volname=cvlabdata,follow_symlinks"
 
 # alias for tracking dotfiles in $HOME folder with git
